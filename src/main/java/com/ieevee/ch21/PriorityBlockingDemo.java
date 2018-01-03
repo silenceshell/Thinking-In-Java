0dc9fb3a5204480be7d4301ddc3d9f3e85d59f56
silenceshell <me@ieevee.com>
2018-01-03 19:18:36 +0800

    update

--- src/main/java/com/ieevee/ch21/PriorityBlockingDemo.java
+++ src/main/java/com/ieevee/ch21/PriorityBlockingDemo.java
@@ -1,5 +1,7 @@
 package com.ieevee.ch21;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
@@ -15,11 +17,15 @@ class PrioritizedTask implements Runnable, Comparable<PrioritizedTask>{
     private static int count;
     private final int id = count++;
     private Random random = new Random(47);
+    private static List<PrioritizedTask> list = new ArrayList<PrioritizedTask>();
+
     PrioritizedTask() {
-        this.priority = 0;
+        this.priority = -1;
     }
+
     PrioritizedTask(int priority) {
         this.priority = priority;
+        list.add(this);
     }
 
     public void run() {
@@ -39,38 +45,65 @@ class PrioritizedTask implements Runnable, Comparable<PrioritizedTask>{
     public int compareTo(PrioritizedTask o) {
         return (this.priority > o.getPriority()) ? -1:
                 ((this.priority < o.getPriority()) ? 1:0);
-
     }
 
     public int getPriority() {
         return priority;
     }
 
-    class EndSentinel extends PrioritizedTask {
-        EndSentinel(int priority) {
-            super(priority);
+    static class EndSentinel extends PrioritizedTask {
+        private ExecutorService service;
+        EndSentinel(ExecutorService service) {
+            this.service = service;
         }
         @Override
         public void run() {
-            super.run();
+            int cnt = 0;
+            System.out.println("---------------------");
+            for (PrioritizedTask task : list) {
+                if (cnt % 5 == 0) {
+                    System.out.println();
+                }
+                System.out.printf("%s  ", task);
+                cnt++;
+            }
+
+            this.service.shutdownNow();
         }
     }
 }
 
 class QueueProducer implements Runnable{
     private PriorityBlockingQueue<Runnable> queue;
+    private ExecutorService service;
     private Random random = new Random(47);
-    QueueProducer(PriorityBlockingQueue<Runnable> queue) {
+    QueueProducer(PriorityBlockingQueue<Runnable> queue, ExecutorService service) {
         this.queue = queue;
+        this.service = service;
     }
 
     public void run() {
         for (int i=0; i< 10; i++) {
-            queue.add(new PrioritizedTask(random.nextInt(100)));
+            queue.add(new PrioritizedTask(random.nextInt(10)));
+            Thread.yield();
+        }
+
+        try {
+            for (int i=0; i< 10; i++) {
+                TimeUnit.MILLISECONDS.sleep(250);
+                queue.add(new PrioritizedTask(10));
+            }
+
+            for (int i=0; i< 10; i++) {
+                queue.add(new PrioritizedTask(i));
+            }
+
+            queue.add(new PrioritizedTask.EndSentinel(this.service));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
-
 
 class QueueConsumer implements Runnable{
     private PriorityBlockingQueue<Runnable> queue;
@@ -80,7 +113,7 @@ class QueueConsumer implements Runnable{
 
     public void run() {
         try {
-            for (int i=0; i< 10; i++) {
+            while (!Thread.interrupted()) {
                 queue.take().run();
             }
         } catch (InterruptedException e ) {
@@ -94,7 +127,7 @@ public class PriorityBlockingDemo {
         ExecutorService exec = Executors.newCachedThreadPool();
         PriorityBlockingQueue<Runnable> queue = new PriorityBlockingQueue<Runnable>();
 
-        exec.execute(new QueueProducer(queue));
+        exec.execute(new QueueProducer(queue, exec));
         exec.execute(new QueueConsumer(queue));
 
     }
